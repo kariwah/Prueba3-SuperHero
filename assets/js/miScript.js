653689aacff4978743f066009d8f0514068a85bc
kariwah <karinav.romero2603@gmail.com>
2025-06-05 12:49:02 -0400

    Mejoras visuales y footer fijo al fondo

--- assets/js/miScript.js
+++ assets/js/miScript.js
@@ -5,29 +5,29 @@
 //no puede acceder a la api de superHero original, ya que el navegador devuelve error CORS, por lo tanto se accede a api superHero de github, con el fin realizar la prueba
 
 $(document).ready(function () {
-    $('.miBtn').on('click', function () {
+  $('.miBtn').on('click', function () {
 
-      if(isNaN($("#buscaSuperHero").val())){
-        //cuando el valor ingresado es false
-        alert("Favor de igresar un numero");
-      }else{
-        //alert("holi");
-        $.ajax({
-            type: "GET",            
-            url: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/" + $("#buscaSuperHero").val() + ".json",
-            success: function (datosApi) {
-                console.log("llegaron los datos");
+    if (isNaN($("#buscaSuperHero").val())) {
+      //cuando el valor ingresado es false
+      alert("Favor de igresar un numero");
+    } else {
+      //alert("holi");
+      $.ajax({
+        type: "GET",
+        url: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/" + $("#buscaSuperHero").val() + ".json",
+        success: function (datosApi) {
+          console.log("llegaron los datos");
 
-                //valida si existe un elemento con clase card creado, se ser true lo elimina y se crea uno nuevo
-                if( $('.card') ){
-                  console.log("card");
-                  $('.card'). remove();
-                }else{
-                  console.log("no card");
-              }
-                //se crean elementos html con datos obtenidos de la api
-                $('.resultado')
-                    .append(`<div class="card mb-3 mt-4 mx-auto" style="max-width: 540px;">
+          //valida si existe un elemento con clase card creado, se ser true lo elimina y se crea uno nuevo
+          if ($('.card')) {
+            console.log("card");
+            $('.card').remove();
+          } else {
+            console.log("no card");
+          }
+          //se crean elementos html con datos obtenidos de la api
+          $('.resultado')
+            .append(`<div class="card mb-3 mt-4 mx-auto" style="max-width: 540px;">
                         <div class="row g-0">
                           <div class="col-md-4">
                             <img src="${datosApi.images.md}" class="img-fluid rounded-start" alt="...">
@@ -49,50 +49,52 @@ $(document).ready(function () {
                         </div>
                       </div>`);
 
-                // se inicializan variables que se usaran en el grafico      
-                var intelligence = datosApi.powerstats.intelligence;
-                var strength = datosApi.powerstats.strength;
-                var speed = datosApi.powerstats.speed;
-                var durability = datosApi.powerstats.durability;
-                var power = datosApi.powerstats.power;
-                var combat = datosApi.powerstats.combat;
-           
-                //se inicia el grafico y se asigna variables que contienen los datos recogidos de la api
-                var chart = new CanvasJS.Chart("chartContainer", {
-                    theme: "light2", // "light1", "light2", "dark1", "dark2"
-                    exportEnabled: true,
-                    animationEnabled: true,
-                    title: {
-                        text: `Estadisticas de poder para ${datosApi.name}`
-                    },
-                    data: [{
-                        type: "pie",
-                        startAngle: 25,
-                        toolTipContent: "<b>{label}</b>: {y}%",
-                        showInLegend: "true",
-                        legendText: "{label}",
-                        indexLabelFontSize: 16,
-                        indexLabel: "{label} - {y}%",
-                        dataPoints: [
-                            { y: intelligence, label: "intelligence" },
-                            { y: strength, label: "strength" },
-                            { y: speed, label: "speed" },
-                            { y: durability, label: "durability" },
-                            { y: power, label: "power" },
-                            { y: combat, label: "combat" }
-                        ]
-                    }]
-                });
-                chart.render();
-            },
-            error: function (error) {
-                //si algo sale mal, se agrega la funcionalidad aquí.
+          // se inicializan variables que se usaran en el grafico      
+          var intelligence = datosApi.powerstats.intelligence;
+          var strength = datosApi.powerstats.strength;
+          var speed = datosApi.powerstats.speed;
+          var durability = datosApi.powerstats.durability;
+          var power = datosApi.powerstats.power;
+          var combat = datosApi.powerstats.combat;
 
-                alert ("No se puede acceder a la api, favor de intertar nuevamente");
-                console.log("no llegaron los datos");
+          //se inicia el grafico y se asigna variables que contienen los datos recogidos de la api
+          var chart = new CanvasJS.Chart("chartContainer", {
+            theme: "light2", // "light1", "light2", "dark1", "dark2"
+            exportEnabled: true,
+            animationEnabled: true,
+            title: {
+              text: `Estadisticas de poder para ${datosApi.name}`
             },
-        });//ajax
-      }//else
-    });//funcion inicio
+            data: [{
+              type: "pie",
+              startAngle: 25,
+              toolTipContent: "<b>{label}</b>: {y}%",
+              showInLegend: "true",
+              legendText: "{label}",
+              indexLabelFontSize: 16,
+              indexLabel: "{label} - {y}%",
+              dataPoints: [
+                { y: intelligence, label: "intelligence" },
+                { y: strength, label: "strength" },
+                { y: speed, label: "speed" },
+                { y: durability, label: "durability" },
+                { y: power, label: "power" },
+                { y: combat, label: "combat" }
+              ]
+            }]
+          });
+          chart.render();
+        },
+        error: function (error) {
+          //si algo sale mal, se agrega la funcionalidad aquí.
+
+          alert("No se puede acceder a la api, favor de intertar nuevamente");
+          console.log("no llegaron los datos");
+        },
+      });//ajax
+    }//else
+  });//funcion inicio
 });//document ready
+
+
 
